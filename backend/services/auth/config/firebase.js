8cebd8d6eb68abe0f1a7c530d74d2b13a4a9a56f
Arpit Srivastava <arpitsri9662@gmail.com>
2026-09-05 16:41:29 +0530

    t fix firebase config for deployment
    :wq#

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -1,6 +1,21 @@
 import { cert, initializeApp } from "firebase-admin";
-import serviceAccount from "../serviceAccountKey.json"  with {type:"json"};
+import fs from "fs";
 
-export const app=initializeApp({
-  credential: cert(serviceAccount)
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    const serviceAccountPath = new URL(
+        "../serviceAccountKey.json",
+        import.meta.url
+    );
+
+    serviceAccount = JSON.parse(
+        fs.readFileSync(serviceAccountPath, "utf8")
+    );
+}
+
+export const app = initializeApp({
+    credential: cert(serviceAccount)
 });
