590b0d3c3a0edd6520f80708230e7f303fe07b6f
Arpit Srivastava <arpitsri9662@gmail.com>
2026-09-06 20:52:25 +0530

    Fix gateway proxy path

--- backend/gateway/utils/proxyWithHeader.js
+++ backend/gateway/utils/proxyWithHeader.js
@@ -2,16 +2,15 @@ import proxy from "express-http-proxy"
 
 export const proxyWithHeader = (serviceUrl) => {
     return proxy(serviceUrl, {
+        proxyReqPathResolver: (req) => {
+            return req.originalUrl.replace(/^\/api\/[^/]+/, "")
+        },
+
         proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
             if (srcReq.user) {
                 proxyReqOpts.headers["x-user-id"] = srcReq.user.userId
             }
             return proxyReqOpts
-        },
-        proxyErrorHandler: (err, res, next) => {
-            console.error("PROXY ERROR:", err.message)
-            console.error("TARGET SERVICE:", serviceUrl)
-            next(err)
         }
     })
 }
